63230a2ddeb0db7384c148353ec547769b08ed98
beshprakash11 <beshprakashktm@gmail.com>
2023-03-16 14:26:54 +0100

    access login screen form main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_tdd/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Text('Flutter Login TDD'),
+      home: const LoginScreen(),
     );
   }
 }
