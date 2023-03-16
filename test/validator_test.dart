d379cb115a739e43c139987b8b733dd228bd2c09
beshprakash11 <beshprakashktm@gmail.com>
2023-03-16 14:40:46 +0100

    email validation , entered field email is realy email or not

--- test/validator_test.dart
+++ test/validator_test.dart
@@ -3,8 +3,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:login_tdd/validator.dart';
 
 void main() {
+
+  //check email validation empty or not
   test(
     'validator for empty email id',
+    () {
+      //ARRANGE & ACCT
+      var result = Validator.validatEmail('');
+
+      //ASSERT
+      expect(result, "Require Field");
+    },
+  );
+
+  //email validation , entered field email is realy email or not
+  test(
+    'validator for invalid email id',
     () {
       //ARRANGE & ACCT
       var result = Validator.validatEmail('');
