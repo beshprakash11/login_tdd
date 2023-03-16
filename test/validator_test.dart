5db860870df7585921a072efff2933568d2997ec
beshprakash11 <beshprakashktm@gmail.com>
2023-03-16 14:36:47 +0100

    email validator created

--- test/validator_test.dart
+++ test/validator_test.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:login_tdd/validator.dart';
 
 void main() {
   test(
     'validator for empty email id',
     () {
-      
+      //ARRANGE & ACCT
+      var result = Validator.validatEmail('');
+
+      //ASSERT
+      expect(result, "require field");
     },
   );
 }
