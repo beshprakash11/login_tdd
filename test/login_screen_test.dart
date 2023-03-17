d3f25c2badee607290c1a68eb133611bbb8717a5
beshprakash11 <beshprakashktm@gmail.com>
2023-03-17 07:14:33 +0100

    Should have on text field from to collect user email id added

--- test/login_screen_test.dart
+++ test/login_screen_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:login_tdd/login_screen.dart';
 
 void main() {
+  //login title test
   testWidgets('Should have a title', (WidgetTester tester) async {
     //ARRANGE
    await tester.pumpWidget(
@@ -14,6 +15,21 @@ void main() {
     );
     //ACT
     Finder title = find.text('Login');
+
+    //ASSERT
+    expect(title, findsOneWidget);
+  });
+
+  //email field test
+  testWidgets('Should have on text field from to collect user email id', (WidgetTester tester) async {
+    //ARRANGE
+   await tester.pumpWidget(
+      MaterialApp(
+        home: LoginScreen(),
+      ),
+    );
+    //ACT
+    Finder title = find.byKey(const ValueKey('email_id'));
 
     //ASSERT
     expect(title, findsOneWidget);
