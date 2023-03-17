4a1bcb04d248ec5bf70b245265716a38c7e784df
beshprakash11 <beshprakashktm@gmail.com>
2023-03-17 07:06:39 +0100

    login screen test added

--- test/login_screen_test.dart
+++ test/login_screen_test.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:login_tdd/login_screen.dart';
 
 void main() {
-  testWidgets('Should have a title', (WidgetTester tester) async{
-    
+  testWidgets('Should have a title', (WidgetTester tester) async {
+    tester.pumpWidget(
+      MaterialApp(
+        home: LoginScreen(),
+      ),
+    );
   });
 }
