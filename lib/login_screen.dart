f88732e4eba30a8cf27f6bfe9cd921e7a60c0b5a
beshprakash11 <beshprakashktm@gmail.com>
2023-03-17 07:11:34 +0100

    appbar added to pass test

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -10,6 +10,10 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Login'),
+      ),
+    );
   }
 }
