2e8195acc4cd5f68eba557bfceba8eaadf045e2d
beshprakash11 <beshprakashktm@gmail.com>
2023-03-17 13:20:09 +0100

    Should show home screen when user taps on login button aftter enter valid email and password updated

--- integration_test/login_flow_test.dart
+++ integration_test/login_flow_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:login_tdd/login_screen.dart';
 
 void main() {
   group(
@@ -9,8 +10,30 @@ void main() {
       IntegrationTestWidgetsFlutterBinding.ensureInitialized();
       testWidgets(
         "Should show home screen when user taps on login button aftter enter valid email and password",
-        (WidgetTester tester) async{
-          
+        (WidgetTester tester) async {
+          // ARRANGE
+          await tester.pumpWidget(
+            const MaterialApp(
+              home: LoginScreen(),
+            ),
+          );
+          //ACT
+          Finder userNameTextField = find.byKey(const ValueKey('email_id'));
+          Finder passwordTextField = find.byKey(const ValueKey('password'));
+
+          //user and password entered
+          await tester.enterText(userNameTextField, "besh@gmail.com");
+          await tester.enterText(passwordTextField, "123456789password");
+
+          //login btn taped
+          Finder loginButton = find.byType(ElevatedButton);
+          await tester.tap(loginButton);
+          await tester.pumpAndSettle();
+
+          Finder errorTexts = find.text("Required Field");
+
+          //ASSERT
+          expect(errorTexts, findsNothing);
         },
       );
     },
