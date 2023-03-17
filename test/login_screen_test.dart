9722ecf1823a86cbb711f940d47f4b5eaab7bebf
beshprakash11 <beshprakashktm@gmail.com>
2023-03-17 07:09:50 +0100

    test title finding updated to find login

--- test/login_screen_test.dart
+++ test/login_screen_test.dart
@@ -1,13 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:login_tdd/login_screen.dart';
 
 void main() {
   testWidgets('Should have a title', (WidgetTester tester) async {
+    //ARRANGE
     tester.pumpWidget(
       MaterialApp(
         home: LoginScreen(),
       ),
     );
+    //ACT
+    Finder title = find.text('Login');
+
+    //ASSERT
+    expect(title, findsOneWidget);
   });
 }
