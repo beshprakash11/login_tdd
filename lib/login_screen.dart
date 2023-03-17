0e296894064e0f0a961468722581f8c7f4249d55
beshprakash11 <beshprakashktm@gmail.com>
2023-03-17 07:19:56 +0100

    email and password controller with dispose added

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -8,11 +8,28 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title:const Text('Login'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+
+          )
+        ],
       ),
     );
   }
